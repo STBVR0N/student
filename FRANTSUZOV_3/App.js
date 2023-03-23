893b3bea5ea92522c96a907c8c0b44645a7ee699
STBVR0N <Max.Frantsuzov@mail.ru>
2023-03-23 11:11:52 +0300

    Add files via upload

---
+++ FRANTSUZOV_3/App.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Get the best prices
+      </Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'white',
+    padding: 8,
+  },
+
+  container1: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'yellow',
+    padding: 8,
+  },
+  
+  paragraph: {
+    margin: 24,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
